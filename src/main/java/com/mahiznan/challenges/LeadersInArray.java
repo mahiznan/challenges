6a0ca85717beedf96982dd3f9bf8c2ef5b0b95f7
Rajeshkumar <rkumar@ctc-g.com.sg>
2021-05-12 18:36:26 +0800

    additional problem solved

---
+++ src/main/java/com/mahiznan/challenges/LeadersInArray.java
@@ -0,0 +1,30 @@
+package com.mahiznan.challenges;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LeadersInArray {
+
+    static ArrayList<Integer> leaders(int arr[], int n) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int currentLeader = arr[n - 1];
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] >= currentLeader) {
+                res.add( arr[i]);
+                currentLeader = arr[i];
+            }
+        }
+       Collections.reverse(res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+//        int[] arr = {1, 3, 5, 2, 2};
+//        int[] arr = {16,17,4,3,5,2};
+        int[] arr = {1, 2, 3, 4, 0};
+        ArrayList<Integer> res = leaders(arr, arr.length);
+        for (Integer re : res) {
+            System.out.println(re);
+        }
+    }
+}
