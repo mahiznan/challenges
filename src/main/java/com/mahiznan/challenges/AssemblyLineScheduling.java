ab14d2e2822d173fbb2db17b1d995dba60361272
Rajeshkumar <rkumar@ctc-g.com.sg>
2021-10-27 13:40:41 +0800

    add additonal problems

---
+++ src/main/java/com/mahiznan/challenges/AssemblyLineScheduling.java
@@ -0,0 +1,27 @@
+package com.mahiznan.challenges;
+
+public class AssemblyLineScheduling {
+
+    static int calculateMinCost(int[][] a, int[][] t, int[] e, int[] x, int s) {
+
+        int[] L1 = new int[s];
+        int[] L2 = new int[s];
+
+        L1[0] = e[0] + a[0][0];
+        L2[0] = e[1] + a[1][0];
+
+        for (int i = 1; i < s; i++) {
+            L1[i] = Math.min(L1[i - 1] + a[0][i], L2[i - 1] + t[1][i] + a[0][i]);
+            L2[i] = Math.min(L2[i - 1] + a[1][i], L1[i - 1] + t[0][i] + a[1][i]);
+        }
+        return Math.min(L1[s - 1] + x[0], L2[s - 1] + x[1]);
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{4, 5, 3, 2}, {2, 10, 1, 4}};
+        int[][] t = {{0, 7, 4, 5}, {0, 9, 2, 8}};
+        int[] e = {10, 12};
+        int[] x = {18, 7};
+        System.out.println(calculateMinCost(a, t, e, x, a[0].length));
+    }
+}
