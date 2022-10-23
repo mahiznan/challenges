a5b29bc6133cd0b7ce2e0ef5ed73ebedfe6833ee
Rajeshkumar <rajeshmepco@gmail.com>
2022-10-23 11:54:24 +0800

    Rearange the pacakges

--- src/main/java/com/mahiznan/challenges/BinaryTree.java
+++ src/main/java/com/mahiznan/challenges/BinaryTree.java
@@ -1,7 +1,7 @@
 /*
-You need to construct a binary tree from a string consisting of parenthesis and integers.
+You need to construct a binary node from a string consisting of parenthesis and integers.
 
-The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the root’s value and a pair of parenthesis contains a child binary tree with the same structure.
+The whole input represents a binary node. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the root’s value and a pair of parenthesis contains a child binary node with the same structure.
 
 You always start to construct the left child node of the parent first if it exists.
 
@@ -17,123 +17,85 @@ Output:
 package com.mahiznan.challenges;
 
 
+import com.mahiznan.util.Node;
+
 import java.util.Stack;
-
-class Tree {
-    private final int value;
-    private Tree leftChild;
-    private Tree rightChild;
-
-    Tree(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public Tree getLeftChild() {
-        return leftChild;
-    }
-
-    public void setLeftChild(Tree leftChild) {
-        this.leftChild = leftChild;
-    }
-
-    public Tree getRightChild() {
-        return rightChild;
-    }
-
-    public void setRightChild(Tree rightChild) {
-        this.rightChild = rightChild;
-    }
-
-    public boolean hasLeftChild() {
-        return this.leftChild != null;
-    }
-
-    public boolean hasRightChild() {
-        return this.rightChild != null;
-    }
-}
 
 
 public class BinaryTree {
-
-
-    static void printTreeTest(Tree tree) {
-        System.out.print(tree.getValue());
-        if (tree.hasLeftChild()) {
+    static void printNodeTest(Node node) {
+        System.out.print(node.data);
+        if (node.left != null) {
             System.out.print("(");
-            printTreeTest(tree.getLeftChild());
+            printNodeTest(node.left);
             System.out.print(")");
         }
-        if (tree.hasRightChild()) {
+        if (node.right != null) {
             System.out.print("(");
-            printTreeTest(tree.getRightChild());
+            printNodeTest(node.right);
             System.out.print(")");
         }
 
     }
 
 
-    static void printTree(Tree tree) {
-        System.out.print(tree.getValue());
-        if (tree.hasLeftChild()) {
+    static void printNode(Node node) {
+        System.out.print(node.data);
+        if (node.left != null) {
             System.out.print("(");
-            printTree(tree.getLeftChild());
+            printNode(node.left);
             System.out.print(")");
         }
-        if (tree.hasRightChild()) {
+        if (node.right != null) {
             System.out.print("(");
-            printTree(tree.getRightChild());
+            printNode(node.right);
             System.out.print(")");
         }
     }
 
 
     public static void main(String[] args) {
-        Tree n1 = new Tree(1);
-        Tree n2 = new Tree(2);
-        Tree n3 = new Tree(3);
-        n1.setLeftChild(n2);
-        n1.setRightChild(n3);
-        Tree n4 = new Tree(4);
-        Tree n5 = new Tree(5);
-        n2.setLeftChild(n4);
-        n2.setRightChild(n5);
-        Tree n6 = new Tree(6);
-        Tree n7 = new Tree(7);
-        n3.setLeftChild(n6);
-        n3.setRightChild(n7);
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        n1.left = n2;
+        n1.right = n3;
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        n2.left = n4;
+        n2.right = n5;
+        Node n6 = new Node(6);
+        Node n7 = new Node(7);
+        n3.left = n6;
+        n3.right = n7;
 
-        Tree n8 = new Tree(8);
-        Tree n9 = new Tree(9);
-        n4.setLeftChild(n8);
-        n4.setRightChild(n9);
-        Tree n10 = new Tree(10);
-        Tree n11 = new Tree(11);
-        n5.setLeftChild(n10);
-        n5.setRightChild(n11);
-        Tree n12 = new Tree(12);
-        Tree n13 = new Tree(13);
-        n6.setLeftChild(n12);
-        n6.setRightChild(n13);
-        Tree n14 = new Tree(14);
-        Tree n15 = new Tree(15);
-        n7.setLeftChild(n14);
-        n7.setRightChild(n15);
-        printTreeTest(n1);
-        System.out.println("");
+        Node n8 = new Node(8);
+        Node n9 = new Node(9);
+        n4.left = n8;
+        n4.right = n9;
+        Node n10 = new Node(10);
+        Node n11 = new Node(11);
+        n5.left = n10;
+        n5.right = n11;
+        Node n12 = new Node(12);
+        Node n13 = new Node(13);
+        n6.left = n12;
+        n6.right = n13;
+        Node n14 = new Node(14);
+        Node n15 = new Node(15);
+        n7.left = n14;
+        n7.right = n15;
+        printNodeTest(n1);
+        System.out.println();
 
 
         String s = "1(2(4(8)(9))(5(10)(11)))(3(6(12)(13))(7(14)(15)))";
 //        String s = "1(2)(3)";
-        Tree t = constructTree(s, 0, s.length() - 1);
+        Node t = constructNode(s, 0, s.length() - 1);
         if (t != null) {
-            printTreeTest(t);
-            System.out.println("");
-            printTree(t);
+            printNodeTest(t);
+            System.out.println();
+            printNode(t);
         }
 
     }
@@ -155,7 +117,7 @@ public class BinaryTree {
         return -1;
     }
 
-    private static Tree constructTree(String s, int startIndex, int endIndex) {
+    private static Node constructNode(String s, int startIndex, int endIndex) {
         if (startIndex > endIndex)
             return null;
         //Extract the value
@@ -165,7 +127,7 @@ public class BinaryTree {
         }
         String value = s.substring(startIndex, q);
         System.out.println("V is " + value);
-        Tree tree = new Tree(Integer.parseInt(value));
+        Node node = new Node(Integer.parseInt(value));
         int index = -1;
         if (startIndex + 1 <= endIndex && s.charAt(startIndex + 1) == '(') {
             index = findIndex(s, startIndex + 1, endIndex);
@@ -173,9 +135,9 @@ public class BinaryTree {
         }
 
         if (index != -1) {
-            tree.setLeftChild(constructTree(s, startIndex + 2, index - 1));
-            tree.setRightChild(constructTree(s, index + 2, endIndex - 1));
+            node.left = constructNode(s, startIndex + 2, index - 1);
+            node.right = constructNode(s, index + 2, endIndex - 1);
         }
-        return tree;
+        return node;
     }
 }
