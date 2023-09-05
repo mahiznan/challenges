5055b273ceeb91649be6c88cd5a066222a97243d
Rajeshkumar <rajeshmepco@gmail.com>
2023-09-06 00:14:13 +0800

    Add recursive solution for pattern matching

--- src/main/java/com/mahiznan/faang/RegularExpressionMatcher.java
+++ src/main/java/com/mahiznan/faang/RegularExpressionMatcher.java
@@ -52,7 +52,12 @@ public class RegularExpressionMatcher {
     static boolean isMatch(String s, String p) {
         if (p.isEmpty()) return s.isEmpty();
         boolean match = !s.isEmpty() && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0));
-        return match && isMatch(s.substring(1), p.substring(1));
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return (isMatch(s, p.substring(2)) ||
+                    (match && isMatch(s.substring(1), p)));
+        } else {
+            return match && isMatch(s.substring(1), p.substring(1));
+        }
     }
 
 
