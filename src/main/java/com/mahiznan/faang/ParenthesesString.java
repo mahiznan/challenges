12175a83f5070b783af3a6db19989c0dcb7856bf
Rajeshkumar <rajeshmepco@gmail.com>
2023-09-14 00:28:30 +0800

    Adding new code

---
+++ src/main/java/com/mahiznan/faang/ParenthesesString.java
@@ -0,0 +1,87 @@
+package com.mahiznan.faang;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Generate Parentheses
+Medium
+50
+Given a number n denoting n pairs of parentheses, return all valid expressions using the n pairs of parentheses.
+
+Examples
+n: 2
+Answer: [
+    "(())",
+    "()()",
+]
+n: 3
+Answer: [
+    "((()))",
+    "(()())",
+    "(())()",
+    "()(())",
+    "()()()"
+]
+Testing
+Input Format
+The first line contains an integer ‘T’, denoting the number of test cases.
+
+For each test case, the input has an integer n.
+
+Output Format
+For each test case, the output has multiple lines each denoting a valid parenthesis expression.
+
+Sample Input
+3
+1
+2
+3
+Expected Output
+()
+(())
+()()
+((()))
+(()())
+(())()
+()(())
+()()()
+Constraint
+1 <= T <= 8
+
+1 <= n <= 8
+ */
+public class ParenthesesString {
+
+    static List<String> generateParentheses(int n) {
+
+        return formParentheses(0, 0, n * 2, "", new ArrayList<>());
+    }
+
+    /*
+
+    1. Tech Solution
+
+    2.
+
+     */
+
+
+
+
+    static List<String> formParentheses(int openingCount, int closingCount, int n, String str, List<String> res) {
+        if (n == 0 && openingCount == closingCount) {
+            res.add(str);
+            return res;
+        }
+        formParentheses(openingCount + 1, closingCount, n - 1, str + "(", res);
+        formParentheses(openingCount, closingCount + 1, n - 1, str + ")", res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        List<String> result = generateParentheses(n);
+        System.out.println(result);
+    }
+}
