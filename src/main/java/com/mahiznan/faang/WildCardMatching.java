ee251578e1d04126626d6b8a0509c244c5f563ad
Rajeshkumar <rajeshmepco@gmail.com>
2023-10-07 20:28:57 +0800

    Add new problem

---
+++ src/main/java/com/mahiznan/faang/WildCardMatching.java
@@ -0,0 +1,18 @@
+package com.mahiznan.faang;
+/*
+Implement wildcard matching.
+
+In a wildcard string:
+
+a-z matches the corresponding character.
+? matches any single character.
+* matches any sequence of characters (incl. empty sequence).
+The entire input string should be matched.
+
+The function would take a string s and the wildcard pattern p.
+ */
+public class WildCardMatching {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
