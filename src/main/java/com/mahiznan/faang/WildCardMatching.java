dbe2d399e0acd03d66aeb442dc0f454538ca2c76
Rajeshkumar <rajeshmepco@gmail.com>
2023-10-08 18:37:08 +0800

    Add additonal solutions

--- src/main/java/com/mahiznan/faang/WildCardMatching.java
+++ src/main/java/com/mahiznan/faang/WildCardMatching.java
@@ -1,5 +1,7 @@
 package com.mahiznan.faang;
 
+import java.util.Arrays;
+
 /*
 Implement wildcard matching.
 
@@ -70,7 +72,41 @@ p contains '?', '*' and lowercase english alphabets.
 
  */
 public class WildCardMatching {
-    static boolean isMatch(String s, String p) {
+
+    static boolean isMatch_Better(String s, String p) {
+        int[][] dp = new int[s.length()][p.length()];
+        for (int[] ar : dp)
+            Arrays.fill(ar, -1);
+        return match(s.length() - 1, p.length() - 1, s, p, dp) != 0;
+    }
+
+    static int match(int i, int j, String s, String p, int[][] dp) {
+        if (i < 0 && j < 0)
+            return 1;
+        if (i < 0) {
+            for (int j1 = 0; j1 <= j; j1++) {
+                if (p.charAt(j1) != '*')
+                    return 0;
+            }
+            return 1;
+        }
+        if (j < 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
+            return dp[i][j] = match(i - 1, j - 1, s, p, dp);
+        int ans = 0;
+        if (p.charAt(j) == '*') {
+            for (int a = 0; a <= s.length(); a++) {
+                ans = ans + match(i - a, j - 1, s, p, dp);
+            }
+            return dp[i][j] = ans;
+        }
+        return dp[i][j] = 0;
+    }
+
+    static boolean isMatch_Good(String s, String p) {
         if (s.isEmpty() && p.isEmpty())
             return true;
         if (s.isEmpty()) {
@@ -83,15 +119,42 @@ public class WildCardMatching {
             }
             return isMatch;
         }
-        if(p.isEmpty())
+        if (p.isEmpty())
             return false;
 
         if (s.charAt(0) == p.charAt(0) || p.charAt(0) == '?') {
-            return isMatch(s.substring(1), p.substring(1));
+            return isMatch_Good(s.substring(1), p.substring(1));
         } else if (p.charAt(0) == '*') {
-            return isMatch(s, p.substring(1)) || isMatch(s.substring(1), p.substring(1)) || isMatch(s.substring(1), p);
+            return isMatch_Good(s, p.substring(1)) || isMatch_Good(s.substring(1), p.substring(1)) || isMatch_Good(s.substring(1), p);
         } else
             return false;
+    }
+
+    static boolean isMatch_Best(String s, String p) {
+        int sIndex = 0, pIndex = 0, matchIndex = 0, starIndex = -1;
+
+        while (sIndex < s.length()) {
+            if (pIndex < p.length() && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '?')) {
+                sIndex++;
+                pIndex++;
+            } else if (pIndex < p.length() && p.charAt(pIndex) == '*') {
+                starIndex = pIndex;
+                matchIndex = sIndex;
+                pIndex++;
+            } else if (starIndex != -1) {
+                pIndex = starIndex + 1;
+                matchIndex++;
+                sIndex = matchIndex;
+            } else {
+                return false;
+            }
+        }
+
+        while (pIndex < p.length() && p.charAt(pIndex) == '*') {
+            pIndex++;
+        }
+
+        return pIndex == p.length();
     }
 
     public static void main(String[] args) {
@@ -103,8 +166,10 @@ public class WildCardMatching {
 //        pattern = "a*b?cd";
 //        string = "";
 //        pattern = "*******";
-        string="babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab";
-        pattern="***bba**a*bbba**aab**b";
-        System.out.println(isMatch(string, pattern));
+        string = "babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab";
+        pattern = "***bba**a*bbba**aab**b";
+//        System.out.println(isMatch_Good(string, pattern));
+//        System.out.println(isMatch_Better(string, pattern));
+        System.out.println(isMatch_Best(string, pattern));
     }
 }
