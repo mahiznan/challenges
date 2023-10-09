0f7c73acda1b8187f77aab4493aa147867ca44ac
Rajeshkumar <rajeshmepco@gmail.com>
2023-10-09 22:15:46 +0800

    Add maximum sum array problem

---
+++ src/main/java/com/mahiznan/faang/MaximumKSumArray.java
@@ -0,0 +1,58 @@
+package com.mahiznan.faang;
+
+/*
+Given an array and a number k, find the sum of the subarray that has the maximum sum among all the subarrays of size k.
+
+Example
+Array: [3, 5, 6, 2, 4, 7, 8]
+k: 3
+Here, the subarrays of size k and their sum are:
+[3 5 6] => 14
+[5 6 2] => 13
+[6 2 4] => 12
+[2 4 7] => 13
+[4 7 8] => 19
+Answer: 19
+Testing
+Input Format
+The first line contains an integer ‘T’ denoting the number of test cases.
+
+For each test case, the input contains two lines:
+
+An integer 'n' denoting the size of the array A and 'k' denoting the size of the subarray.
+n space-separated integers denoting elements of the array A.
+Output format
+For each test-cases, the output has a single integer denoting the maximum subarray sum.
+
+Sample Input
+2
+7 3
+3 5 6 2 4 7 8
+6 1
+1 3 3 3 4 4
+Expected Output
+19
+4
+Constraints
+1 <= T <= 100
+
+1 <= n <= 104
+
+1 <= k <= n
+
+1 <= Ai <= 104
+ */
+public class MaximumKSumArray {
+
+    static int maxKSubarraySum(int[] A, int k) {
+        int maxSum = 0;
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 6, 2, 4, 7, 8};
+        int k = 3;
+        System.out.println(maxKSubarraySum(arr, k));
+    }
+}
