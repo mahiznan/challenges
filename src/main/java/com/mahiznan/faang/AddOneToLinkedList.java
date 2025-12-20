7b8adfcc5a6dc56f857e0193dfb5e0e02edbdc8d
Rajeshkumar <rajeshkumar@users-MacBook-Pro.local>
2025-12-20 19:41:33 +0800

    Add optimisitic solution

--- src/main/java/com/mahiznan/faang/AddOneToLinkedList.java
+++ src/main/java/com/mahiznan/faang/AddOneToLinkedList.java
@@ -1,6 +1,17 @@
 package com.mahiznan.faang;
+/*
+Add One to Linked List
+Medium
+50 / 50
+Given a natural number in the form of a linked list, add 1 to it.
 
-import java.util.List;
+Examples
+Linked List: 7→8→9
+Resultant List: 7→9→0
+Linked List: 9→9→9
+Resultant List: 1→0→0→0
+ */
+import java.util.Stack;
 
 class ListNode {
     int data;
@@ -38,13 +49,41 @@ public class AddOneToLinkedList {
         return newHead;
     }
 
+
+    //Approach 2, Optimistic
+    ListNode addOneToListApproach2(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.data);
+            head = head.next;
+        }
+
+        ListNode cur = null, newHead = null;
+        int carry = 1;
+        while (!stack.isEmpty()) {
+            int data = stack.pop();
+
+            ListNode node = new ListNode((data + carry) % 10);
+            node.next = cur;
+            cur = node;
+            carry = (data + carry) / 10;
+        }
+        if (carry == 1) {
+            newHead = new ListNode(1);
+            newHead.next = cur;
+            return newHead;
+        } else return cur;
+    }
+
+
     public static void main(String[] args) {
 
         ListNode head = new ListNode(9);
         head.next = new ListNode(9);
         head.next.next = new ListNode(9);
         AddOneToLinkedList obj = new AddOneToLinkedList();
-        ListNode result = obj.addOneToList(head);
+//        ListNode result = obj.addOneToList(head);
+        ListNode result = obj.addOneToListApproach2(head);
         while (result != null) {
             if (result.next != null)
                 System.out.print(result.data + "->");
