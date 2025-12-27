3e3a15826a597d8dba9b74e2a4a95d5ac8123c02
Rajeshkumar <rajeshmepco@gmail.com>
2025-12-27 20:19:21 +0800

    Add merge sorted array solution

--- src/main/java/com/mahiznan/faang/MergeSortedArray.java
+++ src/main/java/com/mahiznan/faang/MergeSortedArray.java
@@ -2,9 +2,47 @@ package com.mahiznan.faang;
 
 import java.util.Arrays;
 
+/*
+Given two sorted arrays A and B, find the merged sorted array C by merging A and B.
+
+Example:
+A: [1, 2, 3, 4, 4]
+B: [2, 4, 5, 5]
+C: [1, 2, 2, 3, 4, 4, 4, 5, 5]
+Testing
+Input Format
+First-line contains an integer ‘T’ denoting the number of test cases.
+
+For each test case the input has three lines:
+
+Two space-separated integers ‘n’ and ‘m’ denoting the length of the array A and B respectively.
+n space-separated integers denoting the elements of the array A.
+m space-separated integers denoting the elements of the array B.
+Output Format
+For each test-case, the output has a line with n+m space separated integers denoting the elements of the array C.
+
+Sample Input
+2
+5 2
+1 3 3 4 4
+5 6
+6 2
+1 3 3 3 3 4
+9 11
+Expected Output
+1 3 3 4 4 5 6
+1 3 3 3 3 4 9 11
+Constraints
+1 <= T <= 100
+1 <= n, m <= 104
+1 <= Ai, Bi <= 105
+
+
+ */
+
 public class MergeSortedArray {
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void mergeApproach1(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
         int j = n - 1;
         int k = m + n - 1;
@@ -17,16 +55,42 @@ public class MergeSortedArray {
         }
     }
 
+    public int[] mergeApproach2(int[] arr1, int[] arr2) {
+        int m = arr1.length;
+        int n = arr2.length;
+        int[] result = new int[m + n];
+        int i = 0, j = 0, k = 0;
+
+        while (j < n) {
+            if (i < m && arr1[i] < arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+
+        while (i < m) {
+            result[k++] = arr1[i++];
+        }
+
+        return result;
+
+    }
+
     public static void main(String[] args) {
         MergeSortedArray m = new MergeSortedArray();
-//        int[] nums1 = {1,2,3,0,0,0};
+//        int[] nums1 = {1,2,3};
 //        int[] nums2 = {2,5,6};
 //        int[] nums1 = {1,2,3};
 //        int[] nums2 = {};
-        int[] nums1 = {4,5,6,0,0,0};
-        int[] nums2 = {1,2,3};
+//        int[] nums1 = {4, 5, 6, 0, 0, 0};
+//        int[] nums2 = {1, 2, 3};
+//        int[] nums1 = {1, 2, 3, 4, 4};
+//        int[] nums2 = {2, 4, 5, 5};
+        int[] nums1 = {1, 3, 3, 3, 3, 4};
+        int[] nums2 = {9, 11};
 
-        m.merge(nums1, nums1.length-nums2.length,nums2, nums2.length);
-        System.out.println("args = " + Arrays.toString(nums1));
+        int[] result = m.mergeApproach2(nums1, nums2);
+        System.out.println("Result = " + Arrays.toString(result));
     }
 }
